9d440b7e46baf21d39edf3278d4bbdaeb36c1187
FantasmaShy <fantasmasaimon@gmail.com>
2023-08-16 21:17:41 -0400

    Update script.js
    first edit

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-// Update text when it is changed
-document.addEventListener("DOMContentLoaded", function() {
-    const textOverlay = document.getElementById("textOverlay");
+const div = document.createElement('div');
+var globes = [];
+var gloveCounter = document.getElementById("globeCounter");
 
-    textOverlay.addEventListener("input", function() {
-        const newText = textOverlay.innerHTML; // Use innerHTML to preserve line breaks
-        textOverlay.innerHTML = newText;
-    });
-});
+document.addEventListener("DOMContentLoaded", function(){
+
+    newText(0,0,"OMG VORE\nOwO");
+
+    console.log(":)");
+})
+
+function newText(px,py,txt) {
+    var container = document.getElementById("globePoint")
+
+    globes.push(document.createElement("span"));
+    var tempText = globes[globes.length-1];
+    window.d = new Draggable(tempText);
+
+    tempText.classList.add("textglobe");
+    tempText.textContent = txt;
+
+    tempText.setAttribute("contenteditable", "true");
+    tempText.setAttribute("globeNumber", globes.length-1);
+
+    tempText.style.left = px.toString()+"px";
+    tempText.style.top = py.toString()+"px";
+    tempText.style.width = "auto";
+    tempText.style.height = "auto";
+ 
+    container.appendChild(tempText);
+
+    updateGlobes();
+}
+
+function updateGlobes(){
+    gloveCounter.textContent = globes.length;
+}
